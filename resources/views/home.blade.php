cd8375ea760cfd8f3c8ed0ef6cdf8c7a68edf8fc
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2024-11-15 16:46:31 +0545

    First commit of Laravel-Insta

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://ebranch.lili.org/tetons/wp-content/uploads/sites/22/2016/08/Free-Code-Camp.png"
+                class="rounded-circle">
+        </div>
+        <div class="col-9 p-5">
+            <div class="pb-3"><h1 style="font-size: 20px">Instagram</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-bold">
+                instagram.com
+            </div>
+
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas expedita eaque reiciendis ullam odio quos at eligendi minima dolorem? Corrupti!
+            </div>
+
+            <div class="font-bold"><a href="https://www.instagram.com" style="color: #00008B;">www.instagram.com</a></div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1gH-yH1DeBp7cG7SqJch1eLvG-yd1hZiRwA&s">
+        </div>
+        <div class="col-4">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1gH-yH1DeBp7cG7SqJch1eLvG-yd1hZiRwA&s">
+        </div>
+        <div class="col-4">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1gH-yH1DeBp7cG7SqJch1eLvG-yd1hZiRwA&s">
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/layouts/app.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
